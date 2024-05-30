4a18365ea51aaf1430723570758081f11e79913f
ronitjadhav <ronit.jadhav@camptocamp.com>
2024-05-30 17:35:33 +0200

    Fix running of example app

--- apps/examples/vite.config.ts
+++ apps/examples/vite.config.ts
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +9,13 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@geospatial-sdk/geocoding': fileURLToPath(new URL('../../packages/geocoding/dist', import.meta.url)),
+      '@geospatial-sdk/openlayers': fileURLToPath(new URL('../../packages/openlayers/dist', import.meta.url))
     }
+  },
+  optimizeDeps: {
+    include: ['@geospatial-sdk/geocoding', '@geospatial-sdk/openlayers'],
+    exclude: []
   }
-})
+});
