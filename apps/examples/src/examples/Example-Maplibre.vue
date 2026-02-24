d74d4a4a0ace1dce91dde9df8c927a3e91dd0c8c
Florent Gravin <florent.gravin@camptocamp.com>
2026-02-24 16:25:28 +0100

    demo: rework maplibre layers

--- apps/examples/src/examples/Example-Maplibre.vue
+++ apps/examples/src/examples/Example-Maplibre.vue
@@ -11,27 +11,52 @@ const Layers = {
   },
   wms: {
     type: 'wms',
+    id: 'wms-population',
     url: 'https://data.geopf.fr/wms-r/wms',
-    name: 'INSEE.FILOSOFI.POPULATION'
+    name: 'INSEE.FILOSOFI.POPULATION',
+    label: 'Population INSEE',
+    visibility: true,
+    opacity: 0.7,
+    attributions: '© IGN - INSEE',
+    version: 0,
   } as MapContextLayerWms,
   wfs: {
     type: 'wfs',
     url: 'https://data.lillemetropole.fr/geoserver/dsp_ilevia/ows?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0',
     featureType: 'ilevia_traceslignes',
-    label: 'Tracé des lignes de bus',
+    label: 'Lignes de bus Ilevia (WFS)',
     visibility: true,
-    attributions: 'camptocamp',
-    opacity: 0.5
+    opacity: 0.8,
+    attributions: '© MEL - Ilevia',
+    hoverable: true,
+    style: {
+      "stroke-color": 'rgba(0, 200, 100, 0.5)',
+      "stroke-width": 2,
+    },
   },
   geojson: {
     id: 'geojson',
     type: 'geojson',
-    url: 'https://data.lillemetropole.fr/data/ogcapi/collections/roubaix:implantation_des_arceaux_velos_a_roubaix/items?f=geojson&limit=-1'
+    url: 'https://data.lillemetropole.fr/data/ogcapi/collections/roubaix:implantation_des_arceaux_velos_a_roubaix/items?f=geojson&limit=-1',
+    style: {
+      "circle-fill-color": 'rgba(255, 0, 0, 0.5)',
+      "circle-stroke-color": 'rgba(255, 0, 0, 1)',
+      "circle-stroke-width": 2,
+    },
   },
   ogcapi: {
     type: 'ogcapi',
-    url: 'https://data.lillemetropole.fr/data/ogcapi/collections/ilevia:abris_velo/items?f=json&limit=-1',
-    collection: 'ilevia:abris_velo'
+    url: 'https://data.lillemetropole.fr/data/ogcapi/',
+    collection: 'mobilite_et_transport:pm2035_action_sdvelo_pointsdurs',
+    options: {
+      outputFormat: 'application/geo+json',
+      limit: -1,
+    },
+    label: 'Schéma cyclable - points durs (OGC API)',
+    visibility: true,
+    style: {
+      "circle-radius": ["interpolate", ["linear"], ["zoom"], 5, 1,10, 5],
+    },
   }
 }
 
